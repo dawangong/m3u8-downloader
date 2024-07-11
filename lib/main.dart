48458d6d401a11d974bc13728db2fcbf0b5cb25c
dawangong <dawangong@126.com>
2024-07-11 18:27:31 +0800

    feat: tab 1 ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-void main() {
-  runApp(const MyApp());
+// 全局store
+class CounterModel extends Model {
+  int _selectedIndex = 0;
+  final String _downloadDir = "m3u8-download/";
+
+  int get selectedIndex => _selectedIndex;
+  String get downloadDir => _downloadDir;
+
+  void onItemTapped(int index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,14 +21,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'm3u8-downloader',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'm3u8 downloader'),
-    );
+    return ScopedModel<CounterModel>(
+        model: CounterModel(),
+        child: MaterialApp(
+          title: 'm3u8-downloader',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const MyHomePage(title: 'm3u8 downloader'),
+        ));
   }
 }
 
@@ -30,18 +44,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
   static final List<Widget> _widgetOptions = <Widget>[
     const FirstTab(),
     const SecondTab(),
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,26 +59,34 @@ class _MyHomePageState extends State<MyHomePage> {
             style:
                 const TextStyle(color: Colors.white, fontSize: 28), // 设置标题颜色为白色
           )),
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.add,
-              size: 30,
-            ),
-            label: '添加任务',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.download,
-              size: 30,
-            ),
-            label: '下载列表',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+      body: ScopedModelDescendant<CounterModel>(
+        builder: (context, child, model) {
+          return _widgetOptions.elementAt(model.selectedIndex);
+        },
+      ),
+      bottomNavigationBar: ScopedModelDescendant<CounterModel>(
+        builder: (context, child, model) {
+          return BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+                label: '添加任务',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.download,
+                  size: 30,
+                ),
+                label: '下载列表',
+              ),
+            ],
+            currentIndex: model.selectedIndex,
+            onTap: ScopedModel.of<CounterModel>(context).onItemTapped,
+          );
+        },
       ),
     );
   }
@@ -85,18 +99,107 @@ class FirstTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      padding: const EdgeInsets.all(10),
       child: Flex(
         direction: Axis.vertical,
         children: <Widget>[
           Expanded(
-            child: Container(
-              color: Colors.red,
+            child: Column(
+              children: [
+                Container(
+                    color: Colors.white,
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(top: 14),
+                    child: ScopedModelDescendant<CounterModel>(
+                      builder: (context, child, model) {
+                        return Column(children: [
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            child: const Text(
+                              "m3u8链接",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 19),
+                            ),
+                          ),
+                          const TextField(
+                            textAlign: TextAlign.left,
+                            decoration: InputDecoration(
+                              labelText: "",
+                              labelStyle: TextStyle(height: 0),
+                              hintText: "请输入.m3u8结尾的链接 必填",
+                            ),
+                          )
+                        ]);
+                      },
+                    )),
+                Container(
+                    color: Colors.white,
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(top: 14),
+                    child: ScopedModelDescendant<CounterModel>(
+                      builder: (context, child, model) {
+                        return Column(children: [
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            child: const Text(
+                              "文件重命名",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          const TextField(
+                            textAlign: TextAlign.left,
+                            decoration: InputDecoration(
+                              labelText: "",
+                              labelStyle: TextStyle(height: 0),
+                              hintText: "请输入新的文件名称 选填",
+                            ),
+                          )
+                        ]);
+                      },
+                    )),
+                Container(
+                    color: Colors.white,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(top: 40),
+                    child: ScopedModelDescendant<CounterModel>(
+                      builder: (context, child, model) {
+                        return ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            shadowColor: Colors.transparent, // 移除阴影
+                            elevation: 0,
+                          ), // 移除阴影的另一个方法),
+                          child: Text(
+                            "下载文件",
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                // fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                        );
+                      },
+                    ))
+              ],
             ),
           ),
           Container(
-            height: 30.0,
-            color: Colors.green,
-          ),
+              height: 30.0,
+              alignment: Alignment.centerLeft,
+              child: ScopedModelDescendant<CounterModel>(
+                builder: (context, child, model) {
+                  return Text(
+                    '保存至: ${model.downloadDir}',
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(fontSize: 20),
+                  );
+                },
+              )),
         ],
       ),
     );
@@ -126,4 +229,8 @@ class SecondTab extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MyApp());
 }
